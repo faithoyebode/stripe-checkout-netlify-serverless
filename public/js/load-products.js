9cd461559dc9cb7e6aaf21e7862cfdf256192bcc
Faith Oyebode <47439363+faithoyebode@users.noreply.github.com>
2021-08-25 03:19:15 +0100

    completed fourth lesson

--- public/js/load-products.js
+++ public/js/load-products.js
@@ -1,11 +1,33 @@
+const createProductFromTemplate = (item) => {
+    const template = document.querySelector('#product');
+    const product = template.content.cloneNode(true);
+
+    product.querySelector('h2').innerText = item.name;
+    product.querySelector('.description').innerText = item.description;
+    product.querySelector('[name=sku]').value = item.sku;
+    product.querySelector('.price').innerText = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: item.currency,
+    }).format((item.amount / 100).toFixed(2));
+
+    const img = product.querySelector('img');
+    img.src = item.image;
+    img.alt = item.name;
+
+    return product;
+};
+
+
 export const loadProducts = async () => {
     const data = await fetch('/.netlify/functions/get-products')
     .then((res) => res.json())
     .catch((err) => console.error(err));
 
     const products = document.querySelector('.products');
-    const pre =document.createElement('pre');
-    pre.innerText=JSON.stringify(data, null, 2);
+    
+    data.forEach((item) => {
+        const product = createProductFromTemplate(item);
 
-    products.appendChild(pre);
+        products.appendChild(product);
+    });
 }
